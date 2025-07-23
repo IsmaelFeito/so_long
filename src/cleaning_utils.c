b960bdb68f336f265b065fd1bc656eca5cb7188b
Ismael Feito <isfemo15@gmail.com>
2025-07-23 03:44:45 +0200

    check map conditions 2

--- src/cleaning_utils.c
+++ src/cleaning_utils.c
@@ -6,36 +6,42 @@
 /*   By: ifeito-m <ifeito-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 01:10:18 by ifeito-m          #+#    #+#             */
-/*   Updated: 2025/07/22 01:30:21 by ifeito-m         ###   ########.fr       */
+/*   Updated: 2025/07/23 03:43:37 by ifeito-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
+void	free_textures(t_game *game)
+{
+	if (game->object->wall)
+		mlx_destroy_image(game->mlx_ptr, game->object->wall);
+	if (game->object->floor)
+		mlx_destroy_image(game->mlx_ptr, game->object->floor);
+	if (game->object->player)
+		mlx_destroy_image(game->mlx_ptr, game->object->player);
+	if (game->object->exit)
+		mlx_destroy_image(game->mlx_ptr, game->object->exit);
+	if (game->object->coin)
+		mlx_destroy_image(game->mlx_ptr, game->object->coin);
+}
+
 void clean_mlx_resources(t_game *game)
 {
     if (!game->mlx_ptr)
         return;
-        
+    free_textures(game);
     if (game->mlx_wnd)
     {
         mlx_clear_window(game->mlx_ptr, game->mlx_wnd);
         mlx_destroy_window(game->mlx_ptr, game->mlx_wnd);
+		game->mlx_wnd = NULL;
     }
-    
-    // Liberar texturas si existen
-    if (game->object)
-    {
-        if (game->object->wall)
-            mlx_destroy_image(game->mlx_ptr, game->object->wall);
-        // Repetir para otras texturas...
-        free(game->object);
-    }
-    
     if (game->mlx_ptr)
     {
         mlx_destroy_display(game->mlx_ptr);
         free(game->mlx_ptr);
+		game->mlx_ptr = NULL;
     }
 }
 
